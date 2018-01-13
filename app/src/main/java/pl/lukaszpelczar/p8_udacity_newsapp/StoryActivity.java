3c96bd3e9beb0378f6557f435e5842d06210a0ac
lpelczar <lukepelczar@gmail.com>
2018-01-13 16:07:06 +0100

    Hide API key.

--- app/src/main/java/pl/lukaszpelczar/p8_udacity_newsapp/StoryActivity.java
+++ app/src/main/java/pl/lukaszpelczar/p8_udacity_newsapp/StoryActivity.java
@@ -38,9 +38,11 @@ public class StoryActivity extends AppCompatActivity
 
     private static final String LOG_TAG = StoryActivity.class.getName();
 
+    private static final String KEY = BuildConfig.GUARDIAN_API;
     /** URL for stories from Guardian API */
     private static final String GUARDIAN_REQUEST_URL =
-            "https://content.guardianapis.com/search?order-by=newest&q=bitcoin&show-tags=contributor&api-key=f51da863-1553-4b0f-8207-bad009cf42d4";
+            "https://content.guardianapis.com/search?order-by=newest&q=bitcoin&show-tags=contributor&api-key="
+            + KEY;
 
     /**
      * Constant value for the story loader ID. We can choose any integer.
